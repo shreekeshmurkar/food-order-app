f481c8bb98d5774bf4c3a1c2ad3ee22b1182bd7d
shreekeshmurkar <shreekeshmurkar@gmail.com>
2022-10-11 22:15:11 +0530

    added cart context

---
+++ src/store/CartContextProvider.js
@@ -0,0 +1,15 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (props) => {};
+  const removeItemToCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemToCartHandler,
+  };
+  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+};
+export default CartProvider;
