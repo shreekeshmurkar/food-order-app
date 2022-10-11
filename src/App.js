992d3af1058eea25d920e7051e665313f1473cfc
shreekeshmurkar <shreekeshmurkar@gmail.com>
2022-10-11 15:52:15 +0530

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Fragment } from "react";
+import Header from "./Layout/Header";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
-
 export default App;
