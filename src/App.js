e14b168c0ebcc46bf7c3137253fdf6b36e8391c3
shreekeshmurkar <shreekeshmurkar@gmail.com>
2022-10-11 19:44:23 +0530

    Added partial code for Cart component with Modal and Portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
+import Cart from "./Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
